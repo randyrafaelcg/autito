21a5bb548e0324e9bf21eb8d13228f8ffbaa9490
randyrafaelcg <randyrafaelcg@gmail.com>
2021-10-12 21:28:20 -0400

    prueba para rotar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ function PosFinal(comandos) {
   let grilla=[0,0];
   let Vista="N";
   let posF=["0","0","N"]
+  let instruccion;
   if(comandos!="")
   {
     const Seperado=comandos.split("/");
     grilla=Seperado[0].split(",").map(Number);
     Vista=Seperado[1].replace(/[^EONS]+/, "");
+    instruccion=Seperado[2];
+    Vista = Rotar(Vista,instruccion);
     posXY=Seperado[1].split(/[\D]+/)
     posF[0]=posXY[0].toString();
     posF[1]=posXY[1].toString();
@@ -17,7 +20,16 @@ function PosFinal(comandos) {
     return posF.join();
 
   return posF.join();
-    
+}
+
+function Rotar(vista,comando)
+{
+  if(comando == "I")
+  {
+    if(vista=="N")
+      return "O";
+  }
+  else return vista;
 }
 
 export default PosFinal;
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import PosFinal from "./App.js";
 
-describe("Posicion final del autito", () => {
+describe("Posicion final del autito sin comandos para avanzar", () => {
   it("deberia retornar posicion 0,0,N por defecto", () => {
     expect(PosFinal("")).toEqual("0,0,N");
   });
@@ -12,5 +12,14 @@ describe("Posicion final del autito", () => {
   });
   it("deberia retornar posicion 1,1E cuando no hay comandos para moverse", () => {
     expect(PosFinal("1,1/1,1E/0")).toEqual("1,1,E");
+  });
+  it("deberia retornar posicion 2,2S cuando no hay comandos para moverse", () => {
+    expect(PosFinal("5,5/2,2S/0")).toEqual("2,2,S");
+  });
+});
+
+describe("Posicion final del autito con comandos para rotaciones", () => {
+  it("deberia retornar posicion rotada", () => {
+    expect(PosFinal("5,5/0,0N/I")).toEqual("0,0,O");
   });
 });
